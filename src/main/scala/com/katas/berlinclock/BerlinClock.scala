b8a10e01480dd5ccb2c344e2bce6c92a89dc621e
JoeBew42 <joebew42@gmail.com>
2016-09-07 11:49:26 +0200

    extract method

--- src/main/scala/com/katas/berlinclock/BerlinClock.scala
+++ src/main/scala/com/katas/berlinclock/BerlinClock.scala
@@ -4,17 +4,15 @@ object BerlinClock {
   def topLampAt(seconds: Int) = if(seconds % 2 == 0) "O" else "Y"
 
   def topFirstRowLampsAt(hours: Int): String = {
-    return hours / 5 match {
-      case 1 => "ROOO"
-      case 2 => "RROO"
-      case 3 => "RRRO"
-      case 4 => "RRRR"
-      case _ => "OOOO"
-    }
+    return lightsToLamps(hours / 5)
   }
 
   def topSecondRowLampsAt(hours: Int): String = {
-    return hours % 5 match {
+    return lightsToLamps(hours % 5)
+  }
+
+  def lightsToLamps(lights: Int): String = {
+    return lights match {
       case 1 => "ROOO"
       case 2 => "RROO"
       case 3 => "RRRO"
