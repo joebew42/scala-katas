e935277bd8faf01f0fc6650260ae6f5d754da5c2
JoeBew42 <joebew42@gmail.com>
2016-09-07 12:44:32 +0200

    rename method

--- src/main/scala/com/katas/berlinclock/BerlinClock.scala
+++ src/main/scala/com/katas/berlinclock/BerlinClock.scala
@@ -5,19 +5,19 @@ object BerlinClock {
   def topLampAt(seconds: Int) = if(seconds % 2 == 0) "O" else "Y"
 
   def topFirstRowLampsAt(hours: Int): String = {
-    return lightsToLamps(hours / 5)
+    return redLightsToLamps(hours / 5)
   }
 
   def topSecondRowLampsAt(hours: Int): String = {
-    return lightsToLamps(hours % 5)
-  }
-
-  def lightsToLamps(lights: Int): String = {
-    return List.fill(lights)('R').mkString.padTo(4, 'O')
+    return redLightsToLamps(hours % 5)
   }
 
   def bottomFirstRowLampsAt(minutes: Int): String = {
     return yellowLightsToLamps(minutes / 5)
+  }
+
+  def redLightsToLamps(lights: Int): String = {
+    return List.fill(lights)('R').mkString.padTo(4, 'O')
   }
 
   def yellowLightsToLamps(lights: Int): String = {
