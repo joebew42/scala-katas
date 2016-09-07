f573e40b30d3419b66fe212b54e72579b670b331
JoeBew42 <joebew42@gmail.com>
2016-09-07 11:43:25 +0200

    uniform methods

--- src/main/scala/com/katas/berlinclock/BerlinClock.scala
+++ src/main/scala/com/katas/berlinclock/BerlinClock.scala
@@ -4,12 +4,12 @@ object BerlinClock {
   def topLampAt(seconds: Int) = if(seconds % 2 == 0) "O" else "Y"
 
   def topFirstRowLampsAt(hours: Int): String = {
-    hours / 5 match {
-      case 1 => return "ROOO"
-      case 2 => return "RROO"
-      case 3 => return "RRRO"
-      case 4 => return "RRRR"
-      case _ => return "OOOO"
+    return hours / 5 match {
+      case 1 => "ROOO"
+      case 2 => "RROO"
+      case 3 => "RRRO"
+      case 4 => "RRRR"
+      case _ => "OOOO"
     }
   }
 
